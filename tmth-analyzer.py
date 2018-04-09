2037d22a42ffd627bae7deca135e1dae96d05a1f
chalabaj <chalabaj@vscht.cz>
2018-04-09 15:53:06 +0200

    python 10

--- tmth-analyzer.py
+++ tmth-analyzer.py
@@ -12,28 +12,35 @@ import string
 ##############################################
 ##############################################
 ##############################################
+#Constant:
+
 
 def input_check():
-    #global movies
-    molecule = sys.argv[1]   # TMTH or THMS
-    if not (molecule == "tm" or molecule == "th"):
-       print('Wrong system (tm/th), received:  ', molecule)
-       sys.exit(1)
-       
-    movs = sys.argv[2:]      # list of movies to process
-    print('Number of movie files requested:', len(sys.argv)-2)
-
     movies  = []
     lines   = []
     geoms   = []
+
+    molecule = sys.argv[1]   # TMTH or THMS
+    movs = sys.argv[2:]      # list of movies to process
+        
+    if molecule == "tm":
+       lines_per_mol = 17 
+    elif molecule == "th":
+       lines_per_mol = 13
+    else:
+       print('Wrong system (tm/th), received:  ', molecule)
+       sys.exit(1)
+       
+    print('Number of movie files requested:', len(sys.argv)-2)
+
     for mov in movs:
        cwd = os.getcwd()
        movie_path = os.path.join(cwd,mov)
-       #print(movie_path)
+
        if os.path.isfile(movie_path):
          movies.append(movie_path)
-         gc = geoms_check(movies[-1],molecule) #no need to specify order
-         print(gc)
+         gc = geoms_check(movies[-1],lines_per_mol) #no need to specify order
+         #print(gc)
          lines.append(gc[0])
          geoms.append(gc[1])
          print(mov,' OK, Nlines: ', lines[-1], 'Ngeoms: ', geoms[-1])
@@ -48,9 +55,9 @@ def distance_matrix(movies):
     dist_mat = []
     return dist_mat
     
-def geoms_check(mov,molecule):   # checks the integry of movie files                  
+def geoms_check(mov,lines_per_mol):   # checks the integry of movie files                  
     lines = 0
-    geoms = 1
+    geoms = 0
     buf_size = 1024 * 1024
     with open(mov,'r') as f:
      read_f = f.read
@@ -60,6 +67,12 @@ def geoms_check(mov,molecule):   # checks the integry of movie files
         #\n) is left at the end of the string, and is only omitted on the last line of the file
         buf = read_f(buf_size)
     f.close()
+    if not (lines % lines_per_mol):  
+       geoms = lines / lines_per_mol
+    else:
+       print('Nlines is not divisible by lpm: ',lines, lines_per_mol)
+       sys.exit(1)
+    
     return lines,geoms
 
 ##############################################
